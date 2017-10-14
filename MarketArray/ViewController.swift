17ebef1e00f0c2c9c12e43406cae2f9074d58e36
khaiharnn <khaiharnn@gmail.com>
2017-10-14 14:33:07 +0700

    Start Array

---
+++ MarketArray/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MarketArray
+//
+//  Created by Patinya Sathaporncharoenying on 10/14/2560 BE.
+//  Copyright © 2560 khaiharnn. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
