b89bb50d25cf448a7d3608373b5bde2914cbd286
khaiharnn <khaiharnn@gmail.com>
2017-10-14 15:33:42 +0700

    Success

--- MarketArray/ViewController.swift
+++ MarketArray/ViewController.swift
@@ -9,10 +9,41 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    //Explicit
+    var marketArrayString = ["Thai Market"]
+    var answerString = ""
+    
+    @IBOutlet weak var marketTextField: UITextField!
+    
+    
+    @IBOutlet weak var showLabel: UILabel!
+    
+    
+    
+   
+    @IBAction func addButton(_ sender: Any) {
+        let marketAddString = marketTextField.text
+        marketArrayString.append(marketAddString!)
+        print("LengthArray == \(marketArrayString.count)")
+        
+        //easy show text
+        answerString = answerString + "\n" + marketAddString!
+        showLabel.text = answerString
+
+    }
+    
+    
+
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        //show Market in First
+        showLabel.text = marketArrayString[0]
+        answerString = marketArrayString[0]
     }
 
     override func didReceiveMemoryWarning() {
